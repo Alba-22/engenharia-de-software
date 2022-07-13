446c02630621f5900c9afe4245707e103235af99
Paulo Oyama <papaloyama@gmail.com>
2022-07-12 21:51:09 -0300

    Add responsive things

--- mobile/lib/app/modules/login/login_page.dart
+++ mobile/lib/app/modules/login/login_page.dart
@@ -42,6 +42,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
